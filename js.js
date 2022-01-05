ea585c901308d38aa217b5a08de82102264c6684
T0lst0v <serghei.tolstov@gmail.com>
2022-01-05 14:53:33 -0500

    js.js comented

--- js.js
+++ js.js
@@ -1,13 +1,15 @@
-let taskInput = document.getElementById("taskInput");
-let btnAdd = document.getElementById("btnAdd");
-let newTasksCntnr = document.getElementById("newTasks-cntnr");
-let oldTasksCntnr = document.getElementById("oldTasks-cntnr");
+// OLD test Code
+
+const taskInput = document.getElementById("taskInput");
+const btnAdd = document.getElementById("btnAdd");
+const newTasksCntnr = document.getElementById("newTasks-cntnr");
+const oldTasksCntnr = document.getElementById("oldTasks-cntnr");
 // let delItem = document.getElementsByClassName("delete");
 // let delItem = document.querySelector(".delete");
 // let checkTasks = document.querySelector(".checkbox");
 
-let onTasks = [];
-let offTasks = [];
+const onTasks = [];
+const offTasks = [];
 // let userList = document.getElementById("userList");
 
 btnAdd.addEventListener("click", function () {
@@ -21,10 +23,10 @@ btnAdd.addEventListener("click", function () {
     tagA.setAttribute("href", "#");
     tagA.setAttribute("class", "delete");
 
-    let tagInput = document.createDocumentFragment();
+    const tagInput = document.createDocumentFragment();
     tagInput.innerHTML = "<input type='checkbox' class='checkbox' />";
 
-    let tagLabel = document.createElement("label");
+    const tagLabel = document.createElement("label");
     tagLabel.setAttribute("class", "textField");
 
     tagDiv.innerHTML = tagInput.innerHTML;
@@ -39,7 +41,7 @@ btnAdd.addEventListener("click", function () {
 
     taskInput.value = null;
 
-    console.log(onTasks);
+    // console.log(onTasks);
   }
 });
 
@@ -61,7 +63,15 @@ document.body.addEventListener("click", moveTask);
 function moveTask(e) {
   if (e.target.className === "checkbox" && e.target.parentElement.parentElement.id === "newTasks-cntnr") {
     oldTasksCntnr.appendChild(e.target.parentElement);
-    console.log("move");
+
+    // let item = e.target.parentElement.textContent;
+    // let i = onTasks.indexOf(item);
+    // if (i !== -1) {
+    //   array.splice(i, 1);
+    //   offTasks.push(item);
+
+    // onTasks.push(task)
+    // console.log("move");
   } else if (e.target.className === "checkbox" && e.target.parentElement.parentElement.id === "oldTasks-cntnr") {
     newTasksCntnr.appendChild(e.target.parentElement);
     console.log("move back");
@@ -71,6 +81,8 @@ function moveTask(e) {
   //   if (e.parentElement.parentElement.className === "newTasks-cntnr") {
   //       console.log("move");
   // }
-  console.log(e.target.parentElement);
-  console.log(e.target.parentElement.parentElement.id);
+  let item = e.target.parentElement.textContent;
+  console.log(onTasks);
+  console.log(offTasks);
+  // console.log();
 }
